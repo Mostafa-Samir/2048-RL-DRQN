fd0b5a13eee073edfe336dd76c21442060c64a3f
Mostafa Samir <mostafa.3210@gmail.com>
2016-05-24 20:08:26 +0300

    made sure that tflogs directory exists and that it's empty

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, send_from_directory, jsonify, request
 from flask_socketio import SocketIO
+import os
 
 server = Flask(__name__, static_url_path='', static_folder='game')
 #websocket = SocketIO(server)
@@ -18,3 +19,14 @@ def retrieve_experience():
 
 if __name__ == "__main__":
     server.run(debug=True)
+
+    tensorflow_logdir = os.path.join(os.path.dirname(__file__), "tflog")
+    if os.path.exists(tensorflow_logdir):
+        # clear all the logs if any
+        for entry in os.listdir(tensorflow_logdir):
+            entry_path = os.path.join(tensorflow_logdir, entry)
+            if(os.path.isfile(entry_path)):
+                os.unlink(entry_path)
+    else:
+        #craete the directory
+        os.mkdir(tensorflow_logdir)
