b9cb9f029ae73f6cf4075ee249e300c9553e2796
Mostafa Samir <mostafa.3210@gmail.com>
2016-05-26 02:33:35 +0300

    added frontend dashboard

--- game/js/application.js
+++ game/js/application.js
@@ -1,4 +1,43 @@
+"use strict";
+
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  this.MainManager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+
+  let learnCurve = new Charter("learning-curve", "line", "Learning Curve");
+  let scoreChart  = new Charter("score-chart", "line", "Final Scores");
+  let gamesReporter = new ProgressReporter(".progress");
+
+  AI.init(learnCurve, scoreChart, gamesReporter);
 });
+
+
+// UI control for the dashboard
+document.querySelector("span.show-hide").addEventListener('click', function(e) {
+    let self = e.target;
+    let dashboardContent = document.querySelector('.dashboard-content');
+    let state = dashboardContent.style.display;
+
+    if(state === 'none') {
+        dashboardContent.style.display = 'block';
+        self.innerText = 'Hide';
+    }
+    else {
+        dashboardContent.style.display = 'none';
+        self.innerText = 'Show';
+    }
+});
+
+document.querySelector("#start-training").addEventListener('click', function(e) {
+    let self = e.target;
+    let episodesCounter = document.querySelector("input[name='train-episodes']");
+    let episodesCount = parseInt(episodesCounter.value);
+
+    episodesCounter.disabled = true;
+    self.disabled = true;
+
+    AI.train(episodesCount, () => {
+        episodesCounter.disabled = false;
+        self.disabled = false;
+    });
+})
