f951b910fb7880abf8562ab6b9e56c6442acfaf4
Mostafa Samir <mostafa.3210@gmail.com>
2016-05-26 19:08:15 +0300

    fixed serious bug in network assignment + added save/restore methods

--- ai/dfnn.py
+++ ai/dfnn.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 class Layer:
 
-    def __init__(self, input_size=0, output_size=0, copy_from=None):
+    def __init__(self, input_size=0, output_size=0, indx = 0, copy_from=None):
         """
         constructs a layer in a deep fully-connected neural network
 
@@ -17,19 +17,21 @@ class Layer:
             the index of the layer in the neural network
         """
         if copy_from is None:
+            self.name = "layer" + str(indx)
             self.input_size = input_size
             self.output_size = output_size
 
-            self.weights = tf.Variable(tf.truncated_normal([input_size, output_size], stddev=sqrt(2. / input_size)))
-            self.bias = tf.Variable(tf.zeros([output_size]))
+            self.weights = tf.Variable(tf.truncated_normal([input_size, output_size], stddev=sqrt(2. / input_size)), name=self.name + "_weights")
+            self.bias = tf.Variable(tf.zeros([output_size]), name=self.name + "_bias")
         else:
             source = copy_from
 
+            self.name = source.name + "-copy"
             self.input_size = source.input_size
             self.output_size = source.output_size
 
-            self.weights = tf.Variable(source.weights.initialized_value())
-            self.bias = tf.Variable(source.bias.initialized_value())
+            self.weights = tf.Variable(source.weights.initialized_value(), name=self.name + "_weights")
+            self.bias = tf.Variable(source.bias.initialized_value(), name=self.name + "_bias")
 
 
     def __call__(self, X):
@@ -56,7 +58,7 @@ class Layer:
 
         return layer_clone
 
-    def assign_to(self, other):
+    def assign_to(self, other, session):
         """
         assigns the weigts and the biases of the layer to the
         values of another
@@ -65,11 +67,15 @@ class Layer:
         ----------
         other: Layer
             The target layer
+        session: tf.Session
+            a session to run the assignments
         """
 
         if(self.input_size == other.input_size and self.output_size == other.output_size):
             weights_assign = self.weights.assign(other.weights)
             bias_assign = self.bias.assign(other.bias)
+
+            session.run([weights_assign, bias_assign])
         else:
             print "Layer Mismatch!"
 
@@ -105,7 +111,7 @@ class DFCNN:
         if(len(architecture) > 0):
             for i, hidden_nodes in enumerate(architecture[: len(architecture) - 1]):
                 input_size, output_size = architecture[i], architecture[i + 1]
-                self.layers.append(Layer(input_size, output_size))
+                self.layers.append(Layer(input_size, output_size, i))
 
 
     def __call__(self, X):
@@ -126,7 +132,7 @@ class DFCNN:
 
         return output_layer(activations)
 
-    def assign_to(self, other):
+    def assign_to(self, other, session):
         """
         assigns the weigths and biases of the layers to the values of
         another network
@@ -135,10 +141,12 @@ class DFCNN:
         ----------
         other: DFCNN
             the target network
+        session: tf.Session
+            a session to run the assignments
         """
         if(len(self.layers) == len(other.layers)):
             for i in range(len(self.layers)):
-                self.layers[i].assign_to(other.layers[i])
+                self.layers[i].assign_to(other.layers[i], session)
         else:
             print "Network Mismatch!"
 
@@ -153,3 +161,43 @@ class DFCNN:
             nn_clone.layers.append(layer.clone())
 
         return nn_clone
+
+    def save(self, session, save_path):
+        """
+        saves the neural network to disk
+
+        Parameters:
+        ----------
+        session: tf.Session
+            The tensor flow session enclosing the operations
+        save_path: string
+            The path to save the neural network in
+        """
+
+        var_list = []
+
+        for layer in self.layers:
+            var_list.extend([layer.weights, layer.bias])
+
+        saver = tf.train.Saver(var_list)
+        saver.save(session, save_path)
+
+    def restore(self, session, load_path):
+        """
+        restores a saved neural network from disk
+
+        Parameters:
+        ----------
+        session: tf.Session
+            The tensor flow session enclosing the operations
+        load_path: string
+            The path to load the neural network from
+        """
+
+        var_list = []
+
+        for layer in self.layers:
+            var_list.extend([layer.weights, layer.bias])
+
+        saver = tf.train.Saver(var_list)
+        saver.restore(session, load_path);
